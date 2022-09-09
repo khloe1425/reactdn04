0cfbe13400b61efec11b4bbb38890c6effd8c5c4
Nguyen <tranchauthanhnguyen@gmail.com>
2022-09-09 23:34:19 +0700

    Reactbuoi2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import CardRCC from './components/CardRCC/CardRCC';
 import CardRFC from './components/CardRFC/CardRFC';
 import HomeComponent from './components/BTComponent/HomeComponent/HomeComponent';
+import DataBinding from './components/DataBinding/DataBinding';
+import HandleEvent from './components/HandleEvent/HandleEvent';
+import DemoState from './DemoState/DemoState';
 
 //App là component chính của ứng dụng
 //chứa các thành phần component con
@@ -11,12 +14,13 @@ function App() {
     //chỉ được có 1 thẻ bao bên ngoài
     //thẻ jsx
     <div className="App">
-        <HomeComponent/>
+
+        <DemoState/>
+        {/* <HandleEvent/> */}
         
+          {/* <DataBinding/> */}
 
-
-
-
+        {/* <HomeComponent/> */}
        {/* component */}
         {/* <CardRCC></CardRCC>
         <CardRCC/>
